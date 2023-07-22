9bafc5a17451ade36e76210fc78a0222fbd22d6a
acided <homejet2007@gmail.com>
2023-07-22 13:55:35 +0600

    todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+function TodoList() {
+  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask])
+      setNewTask('')
+    }
+  }
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <h1>Todo List</h1>
+        <div className="task-container">
+          <input
+              type="text"
+              value={newTask}
+              onChange={handleChange}
+              placeholder="Введите новую задачу..."
+          />
+          <button onClick={addTask}>Добавить</button>
+        </div>
+        <ul>
+          {tasks.map((task, index) => (
+              <li key={index}>{task}</li>
+          ))}
+        </ul>
+      </div>
+  )
 }
 
-export default App;
+export default TodoList
